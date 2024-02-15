84f97f02a489123b0a916f348ff04f00989c97c6
Xela <91894688+nnist228@users.noreply.github.com>
2024-02-15 19:20:07 +0100

    Delete DZ1/Source directory
    main goes to src, others to lib (for making static library).

--- DZ1/Source/readBits.h
+++
@@ -1,6 +0,0 @@
-#ifndef READBITS_H
-#define READBITS_H
-#include <inttypes.h>
- uint32_t read32Bits(const uint8_t**); // Прототип для чтения 32битного значения
- uint16_t read16Bits(const uint8_t**); // Прототип для чтения 16битного значения 
-#endif
